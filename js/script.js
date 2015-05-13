a700cc26781a1f771ac5e248e7e9cd095b484758
rodchild <rodchild@gmail.com>
2015-05-13 17:49:07 -0400

    add define styles for mobile devices and change listview display

--- js/script.js
+++ js/script.js
@@ -10,8 +10,12 @@ var WIKI_ENDPOINT = "http://en.wikipedia.org/w/api.php?";
 var markerArray = []; //will contain all the marker for managing the marker toggling.
 var infowindow = new google.maps.InfoWindow(); //this will hold content for each marker infowindow.
 
-//this function will hold the observables for markers and manage the wiki request for every marker
-mapMarker = function (name, lat, lon) {
+/* ============================================*/
+/*              viewModel           */
+/* ============================================*/
+
+/* this function will hold the observables for markers and manage the wiki request for every marker */
+viewModel = function(name, lat, lon) {
     var self = this;
 
     var marker; //to poupulate the map with marker
@@ -31,7 +35,7 @@ mapMarker = function (name, lat, lon) {
     markerArray.push(marker);
 
     var url = WIKI_ENDPOINT + "action=opensearch&search=" + name + "&format=json&callback=wikiCallback";
-    var wikiRequestTimeout = setTimeout(function () {
+    var wikiRequestTimeout = setTimeout(function() {
         contentString = "<div class='markerInfoWindow noscroll'><h4>" + "Failed to get wikipedia resources" + "</h4></div>";
     }, 3000);
     //Wikipedia ajax() request
@@ -41,9 +45,9 @@ mapMarker = function (name, lat, lon) {
         $.ajax({
             url: url,
             dataType: 'jsonp',
-            success: function (data) {
+            success: function(data) {
 
-                contentString = "<aside class='markerInfoWindow noscroll'><header>" + '<h4>' + marker.title + '</h4>' + "</header><hr><p><h5>"  + data[2] + "</h5></p></aside>";
+                contentString = "<aside class='markerInfoWindow noscroll'><header>" + '<h4>' + marker.title + '</h4>' + "</header><hr><p><h5>" + data[2] + "</h5></p></aside>";
                 infoWindow = new google.maps.InfoWindow({
                     content: contentString,
 
@@ -58,7 +62,7 @@ mapMarker = function (name, lat, lon) {
 
     self.isVisible = ko.observable(true);
 
-    self.isVisible.subscribe(function (currentState) {
+    self.isVisible.subscribe(function(currentState) {
         if (currentState) {
             marker.setMap(map);
         } else {
@@ -67,9 +71,9 @@ mapMarker = function (name, lat, lon) {
     });
 
     self.isVisible(true);
-    //here this event listener wait for a marker click, if clicked the map will center based on marker position
-    //marker will starts bouncing and infowindow will open with its content.
-    google.maps.event.addListener(marker, 'click', function () {
+    /* here this event listener wait for a marker click, if clicked the map will center based on marker position */
+    /* marker will starts bouncing and infowindow will open with its content. */
+    google.maps.event.addListener(marker, 'click', function() {
         map.setCenter(marker.getPosition());
         markerToggle(marker);
         infoWindow.setContent(contentString);
@@ -78,9 +82,15 @@ mapMarker = function (name, lat, lon) {
     return false;
 };
 
+/* show and hide listview when mouse hover on search box */
+$( ".form-control" ).hover(function() {
+        $('.container-fluid').show();
+    }, function() {
+        $('.container-fluid').hide();
+    });
 
-// toggles wiki info window visibility status
-//if a marker is clicked it will start bouncing
+/* toggles wiki info window visibility status */
+/* if a marker is clicked it will start bouncing */
 function markerToggle(marker) {
     for (var i = 0; i < markerArray.length; i++) {
         if (i != markerArray[markerArray.indexOf(marker)]) {
@@ -90,39 +100,36 @@ function markerToggle(marker) {
     markerArray[markerArray.indexOf(marker)].setAnimation(google.maps.Animation.BOUNCE);
 }
 
-/* ============================================*/
-/*              viewModel           */
-/* ============================================*/
 
-var viewModel = {
+var mapMarker = {
 
     markers: [
-        new mapMarker("Canada Aviation and Space Museum", 45.4343347, -75.574817113),
-        new mapMarker("La Cité collégiale", 45.439544, -75.626530),
-        new mapMarker("Canada Science and Technology Museum", 45.4081887, -75.6371472),
-        new mapMarker("Gloucester High School", 45.438373, -75.599555),
-        new mapMarker("St. Laurent Centre", 45.421713, -75.639227),
-        new mapMarker("Canadian Museum of Nature", 45.3792579, -75.5785937),
-        new mapMarker("Lester B. Pearson Catholic High School", 45.443309, -75.595915),
-        new mapMarker("Gloucester Public Library", 45.428169, -75.604758),
-        new mapMarker("Montfort Hospital", 45.445432, -75.63906),
-        new mapMarker("The Children's Hospital of Eastern Ontario", 45.401004, -75.651373),
+        new viewModel("Canada Aviation and Space Museum", 45.4343347, -75.574817113),
+        new viewModel("La Cité collégiale", 45.439544, -75.626530),
+        new viewModel("Canada Science and Technology Museum", 45.4081887, -75.6371472),
+        new viewModel("Gloucester High School", 45.438373, -75.599555),
+        new viewModel("St. Laurent Centre", 45.421713, -75.639227),
+        new viewModel("Canadian Museum of Nature", 45.3792579, -75.5785937),
+        new viewModel("Lester B. Pearson Catholic High School", 45.443309, -75.595915),
+        new viewModel("Gloucester Public Library", 45.428169, -75.604758),
+        new viewModel("Montfort Hospital", 45.445432, -75.63906),
+        new viewModel("The Children's Hospital of Eastern Ontario", 45.401004, -75.651373),
     ],
 
     query: ko.observable('')
 };
 
-//searchbox management and location display
-viewModel.markersToShow = ko.pureComputed(function () {
+/* searchbox management and location display */
+mapMarker.markersToShow = ko.pureComputed(function() {
     var self = this;
     search = self.query().toLowerCase();
-    return ko.utils.arrayFilter(viewModel.markers, function (marker) {
+    return ko.utils.arrayFilter(mapMarker.markers, function(marker) {
         var found = marker.name().toLowerCase().indexOf(search) >= 0;
         marker.isVisible(found);
         return found;
     });
-}, viewModel);
+}, mapMarker);
 
 
 
-ko.applyBindings(viewModel);
+ko.applyBindings(mapMarker);
